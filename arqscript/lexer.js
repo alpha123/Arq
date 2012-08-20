4e99431716db295f329b495107b1d86f18f53fe5
Peter Cannici <turkchess123@gmail.com>
2012-08-19 18:53:25 -0600

    ArqScript lexical analyzer

---
+++ arqscript/lexer.js
@@ -0,0 +1,128 @@
+// Unfortunately, Sphere 1.6 doesn't have the JS version set to 170+, so `yield`
+// isn't actually a keyword. Here I work around that to function the same regardless.
+// Bizarrely, even in 170+, we can defined a function called yield. I use that to work
+// around this in the following manner:
+// - Test if we have yield as a keyword or not
+// - Define a function called yield that just pushes its argument into an array
+// - Call yield with parentheses so that it parses regardless of the JS version
+// - If we do have yield as a keyword, it will do the normal behavior
+// - Otherwise, push what would be yielded into a tokens array
+// - If yield is not a keyword, return a generator expression iterating over that array
+
+var hasYield = (function () {
+    try {
+	eval('yield = 42');
+	return false;
+    }
+    catch (e) {
+	return true;
+    }
+})();
+
+exports.tokenizer = function (string, prefix, suffix) {
+    var current, line = 1, from, index = 0, num, str, tokens = [];
+
+    function yield(value) {  // I'm not sure how this is legal, but I'm fortunate that it is.
+	tokens.push(value);
+    }
+
+    prefix = prefix || '<>:/';
+    suffix = suffix || '=>';
+
+    function token(type, value) {
+	return {
+	    type: type,
+	    value: value,
+	    line: line,
+	    from: from,
+	    to: index
+	};
+    }
+
+    function advance() {
+	current = string[++index];
+    }
+
+    current = string[0];
+    while (current) {
+	from = index;
+	if (current == '\n' || (current == '\r' && string[index + 1] == '\n')) {
+	    ++line;
+	    while (current == '\n' || current == '\r')
+		advance();
+	    yield(token('operator', ';'));
+	}
+	else if (current <= ' ')
+	    advance();
+	else if (/[a-zA-Z$]/.test(current)) {
+	    str = current;
+	    advance();
+	    while (current && /[a-zA-Z0-9\-_$?!]/.test(current)) {
+		str += current;
+		advance();
+	    }
+	    yield(token('identifier', str.toUpperCase()));  // Case-insensitive
+	}
+	else if (current >= '0' && current <= '9') {
+	    str = current;
+	    advance();
+	    while (current && /[0-9,]/.test(current)) {
+		if (current != ',')
+		    str += current;
+		advance();
+	    }
+	    if (current == '.') {
+		do {
+		    str += current;
+		    advance();
+		} while (current && /[0-9]/.test(current));
+	    }
+	    while (current && /[a-zA-Z]/.test(current))  // Number suffixes: 5px
+		advance();  // Ignore
+	    num = +str;
+	    if (isFinite(num))
+		yield(token('number', num));
+	    else
+		throw new Error('Invalid number "' + num + '" at line ' + line);
+	}
+	else if (current == '"') {
+	    str = '';
+	    advance();
+	    while (current != '"') {
+		str += current;
+		advance();
+		if (index >= string.length)
+		    throw new Error('Unterminated string at line ' + line);
+	    }
+	    advance();
+	    yield(token('string', str));
+	}
+	else if (current == '-' && string[index + 1] == '-') {
+	    advance();
+	    advance();
+	    str = '';
+	    while (current && current != '\n' && current != '\r') {
+		str += current;
+		advance();
+	    }
+	    yield(token('comment', str));
+	}
+	else if (prefix.indexOf(current) > -1) {
+	    str = current;
+	    advance();
+	    while (index < string.length && suffix.indexOf(current) > -1) {
+		str += current;
+		advance();
+	    }
+	    yield(token('operator', str));
+	}
+	else {
+	    str = current;
+	    advance();
+	    yield(token('operator', str));
+	}
+    }
+
+    if (!hasYield)
+	return (tokens[t] for (t in tokens) if (isFinite(+t)));
+};
